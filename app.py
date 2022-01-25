ac190c9a04d4201c9bfb37e09f9702461e791b01
lovelyoyrmia <mokalulovelyo@gmail.com>
2022-01-25 20:42:51 +0700

    allow origin localhost

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://lovelyoyrmia.github.io"],
+    allow_origins=["https://lovelyoyrmia.github.io", "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
